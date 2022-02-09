653b1acaa6a365826ac472aa6e106b45b5a6ca31
chanceDira <chancedesire60@gmail.com>
2022-02-09 21:05:54 +0200

    create setup react feature heroku-issue7

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,50 @@
-const HtmlWebpackPlugin =require('html-webpack-plugin')
-const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-    context: __dirname,
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/'
+  context: __dirname,
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  devServer: {
+    // contentBase
+    static: {
+      directory: path.join(__dirname, "public/"),
     },
-    devServer: {
-        historyApiFallback: true,
-        static: {
-          directory: path.join(__dirname, 'public'),
-        },
-        compress: true,
-        inline: true,
-        host: '0.0.0.0',
-        port: 8080,
+    port: 8080,
+    // publicPath
+    devMiddleware: {
+      publicPath: "https://localhost:8080/dist/",
+    },
+    // hotOnly
+    hot: "only",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
       },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.css?$/,
-                exclude: /node_modules/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|j?g|svg|gif)?$/,
-                exclude: /node_modules/,
-                use: 'file-loader?name=./assets/[name].[ext]'
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public/index.html'),
-            filename: 'index.html',
-        })
-    ]
-}
+      {
+        test: /\.css?$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|j?g|svg|gif)?$/,
+        exclude: /node_modules/,
+        use: "file-loader?name=./assets/[name].[ext]",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+      filename: "index.html",
+    }),
+  ],
+};
