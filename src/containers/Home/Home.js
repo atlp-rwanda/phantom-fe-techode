5fe38f206ed850d6b11c2375c5945616bcde2329
unknown <sezeranochrisostom123@gmail.com>
2022-02-10 17:22:47 +0200

    migration to traditional redux

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
-import { increment , decrement , incrementByValue } from '../../redux/slices/counter';
+import { decrementBy, decrementCount, incrementBy, incrementCount } from '../../redux/actions/counterActions';
 
 const Home = () => {
-    const { count } = useSelector((state) => state.counter);
-    const dispatch = useDispatch();
-    return (  
+   const { count } = useSelector((state) => state.counters);
+   const dispatch = useDispatch();
+    return (
         <div>
             <h2>Home</h2>
             <div>Redux setup</div>
             <span>{ count }</span>
             <div className="btn">                
-               <button onClick={() => dispatch(increment())}  > Incriment </button>
-               <button onClick={() => dispatch(decrement())} > Decriment </button>
-               <button onClick={() => dispatch(incrementByValue(3))} > Increment by 3 </button>
+               <button onClick={() => dispatch(incrementCount())}  > Incriment </button>
+               <button onClick={() => dispatch(decrementCount())} > Decriment </button>
+               <button onClick={() => dispatch(incrementBy(3))} > Increment by 3 </button>
+               <button onClick={() => dispatch(decrementBy(2))} > Increment by 2 </button>
             </div>
         </div>
     );
