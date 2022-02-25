51b1ae95942c46129d706b6ea3d5aad4be9f2f30
unknown <sezeranochrisostom123@gmail.com>
2022-02-25 10:04:35 +0200

    Adding skeleton

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import Navbar from './components/Navbars/navbar/Navbar';
 import Home from './containers/Home/Home';
 import RegisterDriver from './containers/RegisterDriverOperator/RegisterDriver';
 import RegisterOperator from './containers/RegisterDriverOperator/RegisterOperator';
+import 'react-toastify/dist/ReactToastify.css';
+import './assets/style/style-skeleton.css';
+import Dashboard from './containers/Dashboard/Dashboard';
+
 const App = () => {
     return (  
        <Router>
            <Switch>
                 <Route path="/" exact component={Home} />
+                <Route path="/dashboard"  component={Dashboard} />
                 <Route path="/drivers"  component={RegisterDriver} />
                 <Route path="/operators"  component={RegisterOperator} />
            </Switch>
