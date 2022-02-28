06fe77ba738970d7982d39eb3caf9325472913a2
Delyce Twizeyimana <d.twizeyima@alustudent.com>
2022-02-28 21:27:18 +0200

    solving conflict

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -14,7 +14,7 @@ describe('<App />', () => {
         expect(wrapper.find(Switch)).toHaveLength(1);
     })
     it('should render <App /> with four <Route />', () => {
-        expect(wrapper.find(Route)).toHaveLength(4);
+        expect(wrapper.find(Route)).toHaveLength(6);
     })
     it('should render <App /> with <Router />', () => {
         expect(wrapper.find(Router)).toHaveLength(1);
