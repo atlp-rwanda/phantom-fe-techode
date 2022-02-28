dacd147cf873c9d1925281b3c36ca45dc8b86187
Delyce Twizeyimana <d.twizeyima@alustudent.com>
2022-02-28 21:07:56 +0200

    solving conflict

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './containers/Home/Home';
 import passwordReset from './components/passwordReset/passwordereset';
 import NewPassword from './components/passwordReset/newpassword';
-import navBar from './components/Navbars/navbar/Navbar'
+import Navbar from './components/Navbars/navbar/Navbar'
 
 
 
