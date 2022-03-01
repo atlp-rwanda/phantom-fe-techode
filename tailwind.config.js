09ff6dbe5084bbf643b6e3a020ee5f13a2f665dd
unknown <sezeranochrisostom123@gmail.com>
2022-03-01 11:47:28 +0200

    Delivering requested changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
       ],
     darkMode: false, // or 'media' or 'class'
     theme: {
+        screens: {
+            'sp':'400px',
+            'mp':'500px',
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+          },
         colors:{
             transparent: 'transparent',
             current: 'currentColor',
