4ea3f2a3f0b901bc7c5ff6573ab941d2d1cf3378
unknown <sezeranochrisostom123@gmail.com>
2022-03-09 13:15:54 +0200

    Improving test

--- src/__test__/Profile.test.js
+++ src/__test__/Profile.test.js
@@ -1,51 +1,42 @@
 import React from "react";
 import { shallow } from 'enzyme'
-import renderer from 'react-test-renderer';
-
 import Profile from '../containers/profile/Profile';
-import DashBoardLayout from '../components/dashBoardLayout/DashBoardLayout'
-import SkeletonUpdate from '../components/skeletons/SkeletonUpdate';
-import { OperatorProfile } from '../components/skeletons/cards/Profile';
-import TextField from '../components/fields/TextField'
-import { PrimaryButton } from "../components/buttons/Buttons";
 import store from '../redux/store'
-import { Provider } from 'react-redux'
-
-import configureStore from 'redux-mock-store';
-
-const mockStore = configureStore([]);
-
-
-describe('<Profile />', () => {
-    let wrapper;
-    let store;
-    let component;
+import DashBoardLayout from "../components/dashBoardLayout/DashBoardLayout";
+/* ========== Start::configuration =========== */ 
+import { applyMiddleware , createStore } from 'redux';
+import reducers from '../redux/reducers/index';
+import { middlewares } from "../redux/store";
+import { setProfile } from "../redux/actions/userActions";
+import { OperatorProfile } from "../components/skeletons/cards/Profile";
+/* ===========  End::configuration =========== */ 
 
 
-    
-    beforeEach(()=>{
-      store = mockStore({
-        myState: 'sample text',
-      });
-      component = renderer.create(
-        <Provider store={store}>
-          <Profile email={''} />
-        </Provider>
-      )
-      wrapper = shallow (<Profile />);
-    });
 
-   
-    // it('it should render profile component with layout', () => {
-    //     expect(wrapper.find(DashBoardLayout)).toHaveLength(0)
-    // })
-    // it('it should render profile component with update profile skeleton', () => {
-    //     expect(wrapper.find(OperatorProfile)).toHaveLength(0)
-    // })
-    // it('it should render profile component profile section skeleton', () => {
-    //     expect(wrapper.find(SkeletonUpdate)).toHaveLength(0)
-    // })
-    it('Checks for `Primary button in TextField form component`', () => {
-        expect(component.find(PrimaryButton)).toHaveLength(0);
-    })
+const setUp = (props = {} ) => {
+  const component = shallow(<Profile {...props} store={store}/>).childAt(0);
+  return component;
+}
+const findByClass = (component,nameOfClass) => {
+  const wrapper = component.find(`.${nameOfClass}`);
+  return wrapper;
+} 
+
+
+describe('Profile component ', () => {
+  let component;
+  beforeEach(() => {
+    component = setUp();
+  })
+  test("It should render dashboard layout",() => {
+    // DashBoardLayout
+    let wrapper = component.dive().find(DashBoardLayout);
+    expect(wrapper.length).toBe(1);
+  })  
+  it("It should render OperatorProfile component ",() => {
+    // OperatorProfile
+    let wrapper = component.dive().find(OperatorProfile);
+    expect(wrapper.length).toBe(1);
+  })  
+ 
 })
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
-import { createStore } from "redux";
-import reducers from './reducers/index'
+import { applyMiddleware, createStore } from "redux";
+import reducers from './reducers/index';
+import reduxThunk from 'redux-thunk'
+
+
+export const middlewares = [ reduxThunk ];
+export const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
 
 /* ========= Start: configuration store ========= */ 
-const  store = createStore(reducers , {} , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const  store = createStoreWithMiddleware(reducers , {} , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 /* ========== End: configuration store ========== */ 
 
 export default store
