ef17ffb72317848bd12d130dfb78d5a150df2e0f
wakaflorien <waka.florien45@gmail.com>
2022-03-10 09:36:42 +0200

    assign driver bus layout

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import BusSimulation from './containers/BusSimulation/BusSimulation';
 
 import OperatorAssignBus from './containers/OperatorDriverBus/OperatorAssignBus'
 
+import DashboardOperator from './containers/Dashboard/DashboardOperator'
 
 const App = () => {
     return (  
--- src/containers/RegisterBus/RegisterBus.js
+++ src/containers/RegisterBus/RegisterBus.js
@@ -1,9 +1,17 @@
 import React from "react";
+<<<<<<< HEAD
 import DashboardLayout from "../../components/dashBoardLayout/DashBoardLayout"
 
 const RegisterBus = () => {
     return ( 
         <DashboardLayout></DashboardLayout>
+=======
+import DashboardOpatorLayout from "../../components/dashBoardLayout/DashboardOperatorLayout"
+
+const RegisterBus = () => {
+    return ( 
+        <DashboardOpatorLayout></DashboardOpatorLayout>
+>>>>>>> assign driver bus layout
      );
 }
  
