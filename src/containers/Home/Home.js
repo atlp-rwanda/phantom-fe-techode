6504031c1fca855ab140b35e5d2eed7b034b0cf9
Cyifuzo Jean Damascene <87600918+jeandamascyifuzo@users.noreply.github.com>
2022-05-12 12:06:20 +0200

    ft(landingPage): improve landing page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import Explore from '../../components/Explore/Explore';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
-import HowToStart from '../../components/HowToStart/HowToStart';
 import Intro from '../../components/Intro/Intro';
 import Services from '../../components/Services/Services';
-import Testimonials from '../../components/Testimonials/Testimonials';
 import { decrementBy, decrementCount, incrementBy, incrementCount } from '../../redux/actions/counterActions';
 import "./Home.css"
 
@@ -16,10 +13,7 @@ const Home = () => {
             <div className='h-screen w-full flex-col'>
             <Header />
             <Intro />
-            <HowToStart />
-            <Explore />
             <Services />
-            <Testimonials />
             <Footer />
             </div>
         </div>
